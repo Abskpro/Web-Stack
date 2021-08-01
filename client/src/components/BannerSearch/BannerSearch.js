651e4d9bb4fe1b1587c59c67813ce3a5a0a436e1
adarshmaharjan <40669144+adarshmaharjan@users.noreply.github.com>
2021-08-01 20:18:59 +0545

    [Frontend] corrections

--- client/src/components/BannerSearch/BannerSearch.js
+++ client/src/components/BannerSearch/BannerSearch.js
@@ -1,82 +1,82 @@
-import React, { useState, useEffect } from 'react';
-import mapboxgl from 'mapbox-gl';
-import { connect } from 'react-redux';
-import { displayStuff } from '../../actions/searchAction.js';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
-import { Multiselect } from 'multiselect-react-dropdown'; /**/
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'; /*mapbox css need overide*/
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import mapboxgl from 'mapbox-gl'
+import { connect } from 'react-redux'
+import { displayStuff } from '../../actions/searchAction.js'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder'
+import { Multiselect } from 'multiselect-react-dropdown' /**/
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css' /*mapbox css need overide*/
+import axios from 'axios'
 //react-bootstrap
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap'
 //CSS;
-import '../FormComponent/search.css'; /*search css hatune*/
-import './BannerSearch.css';
+import '../FormComponent/search.css' /*search css hatune*/
+import './BannerSearch.css'
 
 mapboxgl.accessToken =
-	'pk.eyJ1IjoiYWJzazEyMzQiLCJhIjoiY2s3Z3Z3azB6MDQyNzNmbzkxd3MwN3hnNyJ9.-paJt9fSR1rw0Wq0LwSmig';
+	'pk.eyJ1IjoiYWJzazEyMzQiLCJhIjoiY2s3Z3Z3azB6MDQyNzNmbzkxd3MwN3hnNyJ9.-paJt9fSR1rw0Wq0LwSmig'
 
 const BannerSearch = (props) => {
-	const [location, updatelocation] = useState('');
-	const [price, updateprice] = useState(0);
-	const [furnished, updateFurnished] = useState('Not-Furnished');
-	const [type, updatetype] = useState('');
-	const [bedroom, updateBedroom] = useState(1);
-	const [kitchen, updateKitchen] = useState(1);
-	const [toilet, updateToilet] = useState(1);
-	const [livingRoom, updateLivingRoom] = useState(0);
-	const [facilities, updateFacilities] = useState([]);
-	const options = ['CableTv', 'Parking', 'Internet', 'Water-Supply'];
+	const [location, updatelocation] = useState('')
+	const [price, updateprice] = useState(0)
+	const [furnished, updateFurnished] = useState('Not-Furnished')
+	const [type, updatetype] = useState('')
+	const [bedroom, updateBedroom] = useState(1)
+	const [kitchen, updateKitchen] = useState(1)
+	const [toilet, updateToilet] = useState(1)
+	const [livingRoom, updateLivingRoom] = useState(0)
+	const [facilities, updateFacilities] = useState([])
+	const options = ['CableTv', 'Parking', 'Internet', 'Water-Supply']
 
-	const [latestHouse, updateLatestHouse] = useState([]);
-	const [latestRoom, updateLatestRoom] = useState([]);
+	const [latestHouse, updateLatestHouse] = useState([])
+	const [latestRoom, updateLatestRoom] = useState([])
 
 	/*mapbox css*/
 	useEffect(() => {
 		var geocoder = new MapboxGeocoder({
 			accessToken: mapboxgl.accessToken,
 			countries: 'np',
-		});
+		})
 
-		geocoder.addTo('#location');
+		geocoder.addTo('#location')
 		geocoder.on('result', (result) => {
-			console.log(result.result.text);
-			updatelocation(result.result.text);
-		});
+			console.log(result.result.text)
+			updatelocation(result.result.text)
+		})
 		axios.get('/api/search/latest-post').then((data) => {
-			updateLatestRoom(data.data[1]);
-			updateLatestHouse(data.data[0]);
-			console.log(data.data);
-		});
-	}, []);
+			updateLatestRoom(data.data[1])
+			updateLatestHouse(data.data[0])
+			console.log(data.data)
+		})
+	}, [])
 
 	function onChangePreference(e) {
 		if (e.target.name === 'bedroom') {
-			updateBedroom(e.target.value);
+			updateBedroom(e.target.value)
 		} else if (e.target.name === 'kitchen') {
-			updateKitchen(e.target.value);
+			updateKitchen(e.target.value)
 		} else if (e.target.name === 'toilet') {
-			updateToilet(e.target.value);
+			updateToilet(e.target.value)
 		} else {
-			updateLivingRoom(e.target.value);
+			updateLivingRoom(e.target.value)
 		}
 	}
 
 	function onChangeHandler(e) {
 		if (e.target.name === 'furnished') {
-			updateFurnished(e.target.value);
+			updateFurnished(e.target.value)
 		}
 	}
 
 	function onSelect(value) {
-		updateFacilities(value);
-		console.log(facilities);
+		updateFacilities(value)
+		console.log(facilities)
 	}
 
 	function submit(e) {
-		e.preventDefault();
-		console.log(data);
+		e.preventDefault()
+		console.log(data)
 		console.log(
 			location,
 			price,
@@ -87,7 +87,7 @@ const BannerSearch = (props) => {
 			kitchen,
 			toilet,
 			livingRoom
-		);
+		)
 		const data = {
 			location: location,
 			type: type,
@@ -99,17 +99,17 @@ const BannerSearch = (props) => {
 			},
 			facilities: facilities,
 			furnished: furnished,
-		};
-		props.displayStuff(data);
-		props.history.push('/result');
+		}
+		props.displayStuff(data)
+		props.history.push('/result')
 	}
 
 	function loadRoom() {
 		return latestRoom.map((currentRoom, index) => {
 			return (
 				<Posts post={currentRoom} key={currentRoom._id} props={props} />
-			);
-		});
+			)
+		})
 	}
 
 	function loadHouse() {
@@ -120,8 +120,8 @@ const BannerSearch = (props) => {
 					key={currentHouse._id}
 					props={props}
 				/>
-			);
-		});
+			)
+		})
 	}
 
 	return (
@@ -145,7 +145,9 @@ const BannerSearch = (props) => {
 											<div className='search-description'>
 												Location
 											</div>
-											<div id='location' />
+											<div className='search-box-content'>
+												<div id='location' />
+											</div>
 										</div>
 									</Col>
 									<Col lg='2' sm='6' xs='6'>
@@ -153,88 +155,97 @@ const BannerSearch = (props) => {
 											<div className='search-description'>
 												Property Type
 											</div>
-											<select
-												id='type'
-												value={type}
-												onChange={(e) =>
-													updatetype(e.target.value)
-												}
-											>
-												<option value='house'>
-													House
-												</option>
-												<option value='room'>
-													Room
-												</option>
-											</select>
+
+											<div className='search-box-content'>
+												<select
+													id='type'
+													value={type}
+													onChange={(e) =>
+														updatetype(
+															e.target.value
+														)
+													}
+												>
+													<option value='house'>
+														House
+													</option>
+													<option value='room'>
+														Room
+													</option>
+												</select>
+											</div>
 										</div>
 									</Col>
 
 									<Col lg='2' sm='6' xs='6'>
-										<div className='search-box dropdown'>
+										<div className='search-box '>
 											<div className='search-description'>
 												Preference
 											</div>
-											<button
-												className='dropbtn'
-												onClick={(e) =>
-													e.preventDefault()
-												}
-											>
-												Preference
-											</button>
-											<div className='dropdown-content'>
-												<div className='flex-content-pref'>
-													<label>Bedroom</label>
-													<input
-														type='number'
-														name='bedroom'
-														value={bedroom}
-														onChange={
-															onChangePreference
-														}
-														max='5'
-														min='1'
-													/>
-												</div>
-												<div className='flex-content-pref'>
-													<label>Livingroom</label>
-													<input
-														type='number'
-														name='livingroom'
-														value={livingRoom}
-														onChange={
-															onChangePreference
-														}
-														max='2'
-														min='0'
-													/>
-												</div>
-												<div className='flex-content-pref'>
-													<label>Kitchen</label>
-													<input
-														type='number'
-														value={kitchen}
-														name='kitchen'
-														onChange={
-															onChangePreference
-														}
-														max='2'
-														min='1'
-													/>
-												</div>
-												<div className='flex-content-pref'>
-													<label>Toilet</label>
-													<input
-														type='number'
-														value={toilet}
-														name='toilet'
-														onChange={
-															onChangePreference
-														}
-														max='2'
-														min='1'
-													/>
+											<div className='search-box-content dropdown'>
+												<button
+													className='dropbtn'
+													onClick={(e) =>
+														e.preventDefault()
+													}
+												>
+													Preference
+												</button>
+												<div className='dropdown-content'>
+													<div className='flex-content-pref'>
+														<label>Bedroom</label>
+														<input
+															type='number'
+															name='bedroom'
+															value={bedroom}
+															onChange={
+																onChangePreference
+															}
+															max='5'
+															min='1'
+														/>
+													</div>
+													<div className='flex-content-pref'>
+														<label>
+															Livingroom
+														</label>
+														<input
+															type='number'
+															name='livingroom'
+															value={livingRoom}
+															onChange={
+																onChangePreference
+															}
+															max='2'
+															min='0'
+														/>
+													</div>
+													<div className='flex-content-pref'>
+														<label>Kitchen</label>
+														<input
+															type='number'
+															value={kitchen}
+															name='kitchen'
+															onChange={
+																onChangePreference
+															}
+															max='2'
+															min='1'
+														/>
+													</div>
+													<div className='flex-content-pref'>
+														<label>Toilet</label>
+														<input
+															type='number'
+															value={toilet}
+															name='toilet'
+															onChange={
+																onChangePreference
+															}
+															max='2'
+															min='1'
+														/>
+													</div>
 												</div>
 											</div>
 										</div>
@@ -244,12 +255,14 @@ const BannerSearch = (props) => {
 											<div className='search-description'>
 												Facilities
 											</div>
-											<Multiselect
-												options={options}
-												isObject={false}
-												onSelect={onSelect}
-												onRemove={onSelect}
-											/>
+											<div className='search-box-content'>
+												<Multiselect
+													options={options}
+													isObject={false}
+													onSelect={onSelect}
+													onRemove={onSelect}
+												/>
+											</div>
 										</div>
 									</Col>
 									<Col lg='2' sm='6' xs='6'>
@@ -258,24 +271,26 @@ const BannerSearch = (props) => {
 												Furnished ?
 											</div>
 
-											<select
-												value={furnished}
-												onChange={(e) =>
-													updateFurnished(
-														e.target.value
-													)
-												}
-											>
-												<option value='FullyFurnished'>
-													FullyFurnished
-												</option>
-												<option value='Semi-Furnished'>
-													Semi-Furnished
-												</option>
-												<option value='Not-Furnished'>
-													Not-Furnished
-												</option>
-											</select>
+											<div className='search-box-content'>
+												<select
+													value={furnished}
+													onChange={(e) =>
+														updateFurnished(
+															e.target.value
+														)
+													}
+												>
+													<option value='FullyFurnished'>
+														FullyFurnished
+													</option>
+													<option value='Semi-Furnished'>
+														Semi-Furnished
+													</option>
+													<option value='Not-Furnished'>
+														Not-Furnished
+													</option>
+												</select>
+											</div>
 										</div>
 									</Col>
 								</Row>
@@ -299,16 +314,16 @@ const BannerSearch = (props) => {
 				</Container>
 			</section>
 		</div>
-	);
-};
+	)
+}
 
 const navigate = (props) => {
-	console.log(props);
+	console.log(props)
 	props.props.history.push({
 		pathname: '/details/',
 		state: props.post,
-	});
-};
+	})
+}
 
 const Posts = (props) => (
 	<Col xs={12} sm={6} md={6} lg={3}>
@@ -333,7 +348,7 @@ const Posts = (props) => (
 			</div>
 		</div>
 	</Col>
-);
+)
 
 // export default BannerSearch;
 
@@ -341,15 +356,15 @@ const mapStateToProps = (state) => ({
 	auth: state.auth,
 	data: state.data,
 	errors: state.errors,
-});
+})
 
 BannerSearch.protoTypes = {
 	displayStuff: PropTypes.func.isRequired,
 	auth: PropTypes.object.isRequired,
 	errors: PropTypes.object.isRequired,
-};
+}
 
 export default connect(
 	mapStateToProps,
 	{ displayStuff }
-)(BannerSearch);
+)(BannerSearch)
--- client/src/components/Profile/UserInfo/UserInfo.jsx
+++ client/src/components/Profile/UserInfo/UserInfo.jsx
@@ -1,143 +1,161 @@
-import React, { useEffect, useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { Form, Button } from "react-bootstrap";
-import { withRouter } from "react-router-dom";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import "./UserInfo.css";
-import axios from "axios";
+import React, { useEffect, useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { Form, Button } from 'react-bootstrap'
+import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import './UserInfo.css'
+import axios from 'axios'
 
 const UserInfo = (props) => {
-  const [info, setInfo] = useState({});
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [number, setNumber] = useState("");
-  const [notificationLocation, setNotificationLocation] = useState("");
-  const [notificationLocationType, setNotificationLocationType] = useState("");
-  const [alertId, setAlertId] = useState("");
+	const [info, setInfo] = useState({})
+	const [name, setName] = useState('')
+	const [email, setEmail] = useState('')
+	const [number, setNumber] = useState('')
+	const [notificationLocation, setNotificationLocation] = useState('')
+	const [notificationLocationType, setNotificationLocationType] = useState('')
+	const [alertId, setAlertId] = useState('')
 
-  useEffect(() => {
-    const fetchInfo = async (props) => {
-      const res = await axios.get(`/api/profile/info/${props.auth.user.id}`);
+	useEffect(
+		() => {
+			const fetchInfo = async (props) => {
+				const res = await axios.get(
+					`/api/profile/info/${props.auth.user.id}`
+				)
 
-      console.log(res.data);
-      setName(res.data.name);
-      setEmail(res.data.email);
-      setInfo(res.data);
-      setNumber(res.data.number);
+				console.log(res.data)
+				setName(res.data.name)
+				setEmail(res.data.email)
+				setInfo(res.data)
+				setNumber(res.data.number)
 
-      const notificationRes = await axios.get(
-        `/api/notify/notification/${props.auth.user.id}`
-      );
-      if (notificationRes.data != null) {
-        setNotificationLocation(notificationRes.data.location);
-        setNotificationLocationType(notificationRes.data.type);
-        setAlertId(notificationRes.data._id);
-      }
-    };
-    fetchInfo(props);
-  }, [props]);
+				const notificationRes = await axios.get(
+					`/api/notify/notification/${props.auth.user.id}`
+				)
+				if (notificationRes.data != null) {
+					setNotificationLocation(notificationRes.data.location)
+					setNotificationLocationType(notificationRes.data.type)
+					setAlertId(notificationRes.data._id)
+				}
+			}
+			fetchInfo(props)
+		},
+		[props]
+	)
 
-  function updateInfo(e) {
-    e.preventDefault();
-    let newInfo = {
-      email: email,
-      name: name,
-      number: number,
-    };
-    axios
-      .put(`/api/profile/updateUser/${props.auth.user.id}`, newInfo)
-      .then((res) =>
-        toast.info(`🦄 ${res.data.message}`, {
-          position: "bottom-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        })
-      )
-      .catch((err) => console.log(err));
-  }
+	function updateInfo(e) {
+		e.preventDefault()
+		let newInfo = {
+			email: email,
+			name: name,
+			number: number,
+		}
+		axios
+			.put(`/api/profile/updateUser/${props.auth.user.id}`, newInfo)
+			.then((res) =>
+				toast.info(`🦄 ${res.data.message}`, {
+					position: 'bottom-right',
+					autoClose: 5000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+				})
+			)
+			.catch((err) => console.log(err))
+	}
 
-  function deleteNotification(e) {
-    //deletes notification based on usrs id
-    axios.delete(`/api/notify/delete/notification/${alertId}`).then((res) => {
-      window.location.reload(false);
-    });
-  }
+	function deleteNotification(e) {
+		//deletes notification based on usrs id
+		axios
+			.delete(`/api/notify/delete/notification/${alertId}`)
+			.then((res) => {
+				window.location.reload(false)
+			})
+	}
 
-  return (
-    <div className="user-info-container">
-      <Form>
-        <Form.Group>
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <br />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <br />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Phone Number</Form.Label>
-          <Form.Control
-            type="text"
-            value={number}
-            onChange={(e) => setNumber(e.target.value)}
-          />
-          <br />
-        </Form.Group>
-        <Button variant="primary" type="submit" onClick={updateInfo}>
-          UPDATE
-        </Button>
-      </Form>
-      {notificationLocation != "" && (
-        <div>
-          <div>Your Active Alert</div>
-          <div className="notifier-flex">
-            {`Location: ${notificationLocation}  Type:${notificationLocationType}`}
-            <button className="delete-notifier" onClick={deleteNotification}>
-              Remove
-            </button>
-          </div>
-        </div>
-      )}
+	return (
+		<div className='user-info-container'>
+			<Form>
+				<Form.Group>
+					<Form.Label>Username</Form.Label>
+					<Form.Control
+						type='text'
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+					/>
+					<br />
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Email address</Form.Label>
+					<Form.Control
+						type='text'
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<br />
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Phone Number</Form.Label>
+					<Form.Control
+						type='text'
+						value={number}
+						onChange={(e) => setNumber(e.target.value)}
+					/>
+					<br />
+				</Form.Group>
+				<div className='' id='user-info-btn-container'>
+					<Button
+						variant='primary'
+						type='submit'
+						onClick={updateInfo}
+					>
+						UPDATE
+					</Button>
+				</div>
+			</Form>
+			{notificationLocation != '' && (
+				<div id='user-info-notification'>
+					<div className='notification-details'>
+						<div>Your Active Alert</div>
+						<div className='notifier-flex'>
+							{`Location: ${notificationLocation}  Type:${notificationLocationType}`}
+						</div>
+					</div>
+					<button
+						className='delete-notifier'
+						onClick={deleteNotification}
+					>
+						Remove
+					</button>
+				</div>
+			)}
 
-      <ToastContainer
-        position="bottom-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-    </div>
-  );
-};
+			<ToastContainer
+				position='bottom-right'
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+			/>
+		</div>
+	)
+}
 
 UserInfo.propTypes = {
-  auth: PropTypes.object.isRequired,
-  errors: PropTypes.object.isRequired,
-};
+	auth: PropTypes.object.isRequired,
+	errors: PropTypes.object.isRequired,
+}
 
 const mapStateToProps = (state) => ({
-  auth: state.auth,
-  errors: state.errors,
-});
+	auth: state.auth,
+	errors: state.errors,
+})
 
-export default connect(mapStateToProps)(withRouter(UserInfo));
+export default connect(mapStateToProps)(withRouter(UserInfo))
